bef8fcc997d0b539661b7fdc75ee46ec11eb1717
Manish <manish.lakhara@tatvasoft.com>
2025-10-18 21:57:19 +0530

    application expose port

--- src/app.py
+++ src/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/version')
 def version():
-    return jsonify({"version": "1.0.1"})
+    return jsonify({"version": "1.2.0"})
 # add route at root with message "Happy Dipawali!!"
 @app.route('/')
 def home():
     return "Happy Dipawali!!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
